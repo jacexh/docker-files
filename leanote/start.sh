38b95b4d3324300c23e0708a494e62d869dda8d0
jacexh <jace@xuh.me>
2016-06-18 21:36:59 +0800

    clean useless dockerfiles

--- leanote/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -f "$MONGO_DBPATH/leanote.0" ]; then
-  mongod --dbpath $MONGO_DBPATH --logpath=$MONGO_LOG/mongodb.log --fork
-  mongorestore -h localhost -d leanote --dir $GOPATH/src/github.com/leanote/leanote/mongodb_backup/leanote_install_data/
-else
-  mongod --dbpath $MONGO_DBPATH --logpath=$MONGO_LOG/mongodb.log --fork
-fi
-revel run github.com/leanote/leanote prod
--- lt.dockerfile
+++
@@ -1,7 +0,0 @@
-FROM jacexh/wosai:ft
-
-MAINTAINER Jace Xu <jace@xuh.me>
-ENV REFRESHED_AT 2015-09-25
-
-EXPOSE 8089
-ENTRYPOINT ["locust"]
